5ee12dace4bc777917437e23ea7863bd5868b769
vanga <pradeepreddy.manu.iitkgp@gmail.com>
2015-07-08 14:58:49 +0530

    Basic version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # elasticsearch-stats-to-cloudwatch
-Send elasticsearch node stats to AWS Cloudwatch
+Send elasticsearch node stats(from marvel) to AWS Cloudwatch
+
+To Run on EC2 with an IAM role
+
+Sends only Heap usage atm
+
+Prerequisites boto,elasticsearch
+```
+pip install boto
+pip install elasticsearch
+```
+
+Configure ES_HOST and region vars
---
+++ es2cw.py
@@ -0,0 +1,94 @@
+#!/bin/python
+from boto.ec2 import cloudwatch
+import json
+
+region = "us-west-1"
+ES_HOST = "localhost"
+## elasticsearch
+from elasticsearch import Elasticsearch
+
+aws = {}
+aws["region"] = "us-west-1"
+
+cw_metric_object = {}
+cw_metric_object["namespace"] = "production_elasticsearch"
+cw_metric_object["name"] = []
+cw_metric_object["value"] = []
+cw_metric_object["unit"] = []
+cw_metric_object["dimensions"] = None
+cw_metric_object["statistics"] = None
+cw_metric_object["timestamp"] = None
+
+
+def put_cloudwatch_metric():
+  cw = cloudwatch.connect_to_region("us-west-1")
+  cw.put_metric_data(cw_metric_object["namespace"],
+  	                 cw_metric_object["name"],
+  	                 value = cw_metric_object["value"],
+  	                 unit = cw_metric_object["unit"],
+  	                 timestamp = cw_metric_object["timestamp"],
+  	                 dimensions = cw_metric_object["dimensions"],
+  	                 statistics = cw_metric_object["statistics"])
+  print "Successfully sent metrics to cloudwatch"
+
+
+
+body = {
+    "query": {
+        "filtered": {
+            "filter": {
+                "range": {
+                    "@timestamp": {
+                        "gte": "now-2m",
+                        "lte": "now"
+                    }
+                }
+            }
+        }
+    },
+    "size": 0,
+    "aggs": {
+        "minutes": {
+            "date_histogram": {
+                "field": "@timestamp",
+                "interval": "day"
+            },
+            "aggs": {
+                "nodes": {
+                    "terms": {
+                        "field": "node.name.raw",
+                        "size": 10,
+                        "order": {
+                            "memory": "desc"
+                        }
+                    },
+                    "aggs": {
+                        "memory": {
+                            "avg": {
+                                "field": "jvm.mem.heap_used_percent"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+def get_metric_data():
+	es = Elasticsearch([{'host' : ES_HOST, 'port' : 9200}])
+	res = es.search(
+		index = ".marvel-*",
+		body = body)
+	heap_stats = res["aggregations"]["minutes"]["buckets"][0]['nodes']['buckets']
+	parsed = json.dumps(heap_stats, indent=4)
+	for node in heap_stats:
+		cw_metric_object["value"].append(node["memory"]["value"])
+		cw_metric_object["name"].append("production_" + node["key"] + "_heap_memory")
+		cw_metric_object["unit"].append("Percent")
+		print "Added metrics for node " + node["key"]
+
+
+get_metric_data()
+put_cloudwatch_metric()
