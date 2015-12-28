05a4fd6d879aaaf9dfc64511493bd1aec67590e9
vanga <pradeepreddy.manu.iitkgp@gmail.com>
2015-12-28 13:13:44 +0530

    Added query for monitoring Search queue length

--- es2cw.py
+++ es2cw.py
@@ -30,17 +30,17 @@ cw_metric_object["timestamp"] = None
 def put_cloudwatch_metric():
   cw = cloudwatch.connect_to_region("us-west-1")
   cw.put_metric_data(cw_metric_object["namespace"],
-  	                 cw_metric_object["name"],
-  	                 value = cw_metric_object["value"],
-  	                 unit = cw_metric_object["unit"],
-  	                 timestamp = cw_metric_object["timestamp"],
-  	                 dimensions = cw_metric_object["dimensions"],
-  	                 statistics = cw_metric_object["statistics"])
+                     cw_metric_object["name"],
+                     value = cw_metric_object["value"],
+                     unit = cw_metric_object["unit"],
+                     timestamp = cw_metric_object["timestamp"],
+                     dimensions = cw_metric_object["dimensions"],
+                     statistics = cw_metric_object["statistics"])
   print "Successfully sent metrics to cloudwatch"
 
 
 
-body = {
+heap_query = {
     "query": {
         "filtered": {
             "filter": {
@@ -82,19 +82,74 @@ body = {
     }
 }
 
+search_queue_query ={
+    "query": {
+        "filtered": {
+            "filter": {
+                "range": {
+                    "@timestamp": {
+                        "gte": "now-2m",
+                        "lte": "now"
+                    }
+                }
+            }
+        }
+    },
+    "size": 0,
+    "aggs": {
+        "minutes": {
+            "date_histogram": {
+                "field": "@timestamp",
+                "interval": "day"
+            },
+            "aggs": {
+                "nodes": {
+                    "terms": {
+                        "field": "node.name.raw",
+                        "size": 10,
+                        "order": {
+                            "searchqueue": "desc"
+                        }
+                    },
+                    "aggs": {
+                        "searchqueue": {
+                            "avg": {
+                                "field": "thread_pool.search.queue"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 def get_metric_data():
-	es = Elasticsearch([{'host' : ES_HOST, 'port' : ES_PORT}])
-	res = es.search(
-		index = ".marvel-*",
-		body = body)
-	heap_stats = res["aggregations"]["minutes"]["buckets"][0]['nodes']['buckets']
-	parsed = json.dumps(heap_stats, indent=4)
-	for node in heap_stats:
-		cw_metric_object["value"].append(node["memory"]["value"])
-		cw_metric_object["name"].append("production_" + node["key"] + "_heap_memory")
-		cw_metric_object["unit"].append("Percent")
-		print "Added metrics for node " + node["key"]
+    es = Elasticsearch([{'host' : ES_HOST, 'port' : ES_PORT}])
+
+    res = es.search(
+        index = ".marvel-*",
+        body = heap_query)
+    heap_stats = res["aggregations"]["minutes"]["buckets"][0]['nodes']['buckets']
+    parsed = json.dumps(heap_stats, indent=4)
+    for node in heap_stats:
+        cw_metric_object["value"].append(node["memory"]["value"])
+        cw_metric_object["name"].append("production_" + node["key"] + "_heap_memory")
+        cw_metric_object["unit"].append("Percent")
+        print "Added metrics for node " + node["key"]
+
+    # search queue monitoring
+    res = es.search(
+        index = ".marvel-*",
+        body = search_queue_query)
+    sq_stats = res["aggregations"]["minutes"]["buckets"][0]['nodes']['buckets']
+    parsed = json.dumps(sq_stats, indent=4)
+    for node in sq_stats:
+        cw_metric_object["value"].append(node["searchqueue"]["value"])
+        cw_metric_object["name"].append("production_" + node["key"] + "_search_queue")
+        cw_metric_object["unit"].append("Count")
+        print "Added metrics for node " + node["key"]
 
 
 get_metric_data()
