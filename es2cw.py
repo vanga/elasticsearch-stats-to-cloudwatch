036ad55af6d0955c1fb10877b94279e218c8cd8e
vanga <pradeepreddy.manu.iitkgp@gmail.com>
2017-02-14 16:26:33 +0530

    Upgraded to ES 5.x

--- es2cw.py
+++ es2cw.py
@@ -3,6 +3,7 @@ from boto.ec2 import cloudwatch
 import json
 import yaml
 import sys
+import time
 ## elasticsearch
 from elasticsearch import Elasticsearch
 
@@ -15,20 +16,13 @@ with open("config/" + env +".yml", 'r') as config_file:
 ES_HOST = config['elasticsearch']['host']
 ES_PORT = config['elasticsearch']['port']
 aws = {}
-aws["region"] = "us-west-1"
+aws_region = config['aws']['region']
+aws["region"] = aws_region
+metrics_to_monitor = ["heap_percent","search_queue","gc_young","gc_old","fd_evicts","qc_evicts"]
 
-cw_metric_object = {}
-cw_metric_object["namespace"] = "production_elasticsearch"
-cw_metric_object["name"] = []
-cw_metric_object["value"] = []
-cw_metric_object["unit"] = []
-cw_metric_object["dimensions"] = None
-cw_metric_object["statistics"] = None
-cw_metric_object["timestamp"] = None
-
-
-def put_cloudwatch_metric():
-  cw = cloudwatch.connect_to_region("us-west-1")
+def put_cloudwatch_metric(cw_metric_object):
+  print cw_metric_object,"cw_metric_object"
+  cw = cloudwatch.connect_to_region(aws_region)
   cw.put_metric_data(cw_metric_object["namespace"],
                      cw_metric_object["name"],
                      value = cw_metric_object["value"],
@@ -39,118 +33,100 @@ def put_cloudwatch_metric():
   print "Successfully sent metrics to cloudwatch"
 
 
-
-heap_query = {
-    "query": {
-        "filtered": {
-            "filter": {
-                "range": {
-                    "@timestamp": {
-                        "gte": "now-2m",
-                        "lte": "now"
-                    }
-                }
-            }
-        }
+metrics = {
+    "heap_percent": {
+        "type": "avg",
+        "path":"node_stats.jvm.mem.heap_used_percent",
+        "unit": "Percent"
     },
-    "size": 0,
-    "aggs": {
-        "minutes": {
-            "date_histogram": {
-                "field": "@timestamp",
-                "interval": "day"
-            },
-            "aggs": {
-                "nodes": {
-                    "terms": {
-                        "field": "node.name.raw",
-                        "size": 10,
-                        "order": {
-                            "memory": "desc"
-                        }
-                    },
-                    "aggs": {
-                        "memory": {
-                            "avg": {
-                                "field": "jvm.mem.heap_used_percent"
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    "search_queue": {
+        "type": "avg",
+        "path": "node_stats.thread_pool.search.queue",
+        "unit": "Count"
+    },
+    "gc_young": {
+        "type": "derivative",
+        "path": "node_stats.jvm.gc.collectors.young.collection_time_in_millis",
+        "unit": "Count"  
+    },
+    "gc_old": {
+        "type": "derivative",
+        "path": "node_stats.jvm.gc.collectors.old.collection_time_in_millis",
+        "unit": "Count"
+    },
+    "fd_evicts": {
+        "type": "derivative",
+        "path": "node_stats.indices.fielddata.evictions",
+        "unit": "Count"
+    },
+    "qc_evicts": {
+        "type": "derivative",
+        "path": "node_stats.indices.query_cache.evictions",
+        "unit": "Count"
     }
 }
 
-search_queue_query ={
-    "query": {
-        "filtered": {
-            "filter": {
-                "range": {
-                    "@timestamp": {
-                        "gte": "now-2m",
-                        "lte": "now"
-                    }
-                }
-            }
-        }
-    },
-    "size": 0,
-    "aggs": {
-        "minutes": {
-            "date_histogram": {
-                "field": "@timestamp",
-                "interval": "day"
-            },
-            "aggs": {
-                "nodes": {
-                    "terms": {
-                        "field": "node.name.raw",
-                        "size": 10,
-                        "order": {
-                            "searchqueue": "desc"
-                        }
-                    },
-                    "aggs": {
-                        "searchqueue": {
-                            "avg": {
-                                "field": "thread_pool.search.queue"
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+def get_es_query(metric):
+    if metrics[metric]["type"] == "avg":
+        es_query = {"query":{"bool":{"must":{"range":{"timestamp":{"gte":"now-2m","lte":"now"}}}}},"size":0,"aggs":{"nodes":{"terms":{"field":"source_node.name","size":1},"aggs":{"metrics":{"date_histogram":{"field":"timestamp","interval":"60s"},"aggs":{metric:{"avg":{"field":metrics[metric]["path"]}}}}}}}}
+    elif metrics[metric]["type"] == "derivative":
+        cur_time = int(time.time() * 1000)
+        prev_time = cur_time - 600000
+        es_query = {"size":0,"query":{"bool":{"filter":[{"range":{"timestamp":{"format":"epoch_millis","gte":prev_time,"lte":cur_time}}}]}},"aggs":{"nodes":{"terms":{"field":"source_node.name"},"aggs":{"metrics":{"date_histogram":{"field":"timestamp","min_doc_count":0,"interval":"60s","extended_bounds":{"min":prev_time,"max":cur_time}},"aggs":{metric:{"max":{"field":metrics[metric]["path"]}},metric+"_deriv":{"derivative":{"buckets_path":metric,"gap_policy":"skip"}}}}}}}}
+    else:
+        raise Exception("Metric is unknown to generate ES query")
+    return es_query
 
+def get_base_cw_metric_object():
+    cw_metric_object = {}
+    cw_metric_object["namespace"] = env+"_elasticsearch"
+    cw_metric_object["name"] = []
+    cw_metric_object["value"] = []
+    cw_metric_object["unit"] = []
+    cw_metric_object["dimensions"] = None
+    cw_metric_object["statistics"] = None
+    cw_metric_object["timestamp"] = None 
+    return cw_metric_object    
 
-def get_metric_data():
+def get_metric_data(metric, cw_metric_object):
+    metric_unit = metrics[metric]['unit']
+    if metrics[metric]["type"] == "avg":
+        value_key = metric
+    elif metrics[metric]["type"] == "derivative":
+        value_key = metric+"_deriv"
     es = Elasticsearch([{'host' : ES_HOST, 'port' : ES_PORT}])
-
+    es_query = get_es_query(metric)
     res = es.search(
-        index = ".marvel-*",
-        body = heap_query)
-    heap_stats = res["aggregations"]["minutes"]["buckets"][0]['nodes']['buckets']
-    parsed = json.dumps(heap_stats, indent=4)
-    for node in heap_stats:
-        cw_metric_object["value"].append(node["memory"]["value"])
-        cw_metric_object["name"].append("production_" + node["key"] + "_heap_memory")
-        cw_metric_object["unit"].append("Percent")
-        print "Added metrics for node " + node["key"]
+        index = ".monitoring-es-*",
+        body = es_query)
+    es_stats = res["aggregations"]["nodes"]["buckets"]
 
+    parsed = json.dumps(es_stats, indent=4)
+    for node_stats in es_stats:
+        print node_stats["metrics"]["buckets"][-1]
+        node_name = node_stats["key"]
+        if value_key not in node_stats["metrics"]["buckets"][-1]:
+            continue
+        metric_value = node_stats["metrics"]["buckets"][-1][value_key]["value"]
+        cw_metric_object["value"].append(metric_value)
+        cw_metric_object["name"].append(env+"_" + node_name + "_"+metric)
+        cw_metric_object["unit"].append(metric_unit)
+        print "Added metrics for node " + node_name,metric+":",metric_value
+    return cw_metric_object
     # search queue monitoring
-    res = es.search(
-        index = ".marvel-*",
-        body = search_queue_query)
-    sq_stats = res["aggregations"]["minutes"]["buckets"][0]['nodes']['buckets']
-    parsed = json.dumps(sq_stats, indent=4)
-    for node in sq_stats:
-        cw_metric_object["value"].append(node["searchqueue"]["value"])
-        cw_metric_object["name"].append("production_" + node["key"] + "_search_queue")
-        cw_metric_object["unit"].append("Count")
-        print "Added metrics for node " + node["key"]
+    # res = es.search(
+    #     index = ".marvel-*",
+    #     body = search_queue_query)
+    # sq_stats = res["aggregations"]["minutes"]["buckets"][0]['nodes']['buckets']
+    # parsed = json.dumps(sq_stats, indent=4)
+    # for node in sq_stats:
+    #     cw_metric_object["value"].append(node["searchqueue"]["value"])
+    #     cw_metric_object["name"].append("production_" + node["key"] + "_search_queue")
+    #     cw_metric_object["unit"].append("Count")
+    #     print "Added metrics for node " + node["key"]
 
 
-get_metric_data()
-put_cloudwatch_metric()
+cw_metric_object = get_base_cw_metric_object()
+for metric in metrics_to_monitor:
+    cw_metric_object = get_metric_data(metric,cw_metric_object)
+    put_cloudwatch_metric(cw_metric_object)
